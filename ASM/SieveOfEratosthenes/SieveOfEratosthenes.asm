6f0c1eeda351631e8146365cd23de0c36cfc2d85
Brian Tabone <brian_tabone@yahoo.com>
2023-12-03 13:36:54 -0600

    Implemented BCD conversion for display of base 10 on 7 segment display

--- ASM/SieveOfEratosthenes/SieveOfEratosthenes.asm
+++ ASM/SieveOfEratosthenes/SieveOfEratosthenes.asm
@@ -49,6 +49,7 @@
     MULTRESH:           equ     $0702
     SAVEX:              equ     $0703
     SAVEY:              equ     $0704
+    BCDVAL:             equ     $04  ; 16 bits 04 and 05 to hold 4 BCD digits (max 9999)
 		CHIP	65C02
 		LONGI	OFF
 		LONGA	OFF
@@ -64,6 +65,9 @@
         LDA #$00
         STA CNTL        ; Initialize counter mem
         STA CNTH
+        STA BCDVAL
+        STA BCDVAL+1
+
     ; First, Turn off all of the LEDs
 		lda	#$00
 		sta	LED_IO_ADDR	; Turn off the LEDs
@@ -151,7 +155,13 @@
         CMP #$00
         BEQ DISP_LOOP ; Skip over the 0s
         STA LED_IO_ADDR
-        STA SEVEN_SEG_IO_ADDR ; Also display hex (for now) on seven segment display. Will try BCD later
+        
+        JSR BINTOBCD ; Convert what is in A to BCD, function will manage BCD
+        LDA BCDVAL
+        STA SEVEN_SEG_IO_ADDR 
+        LDA BCDVAL+1
+        STA SEVEN_SEG_IO_ADDR+1
+        
         JSR SAVEXY  ; For now X and Y not touched by counter, but better to be safe
         JSR COUNTER ; Run the counter for some processor delay
         JSR RESTXY
@@ -190,7 +200,44 @@
             JMP COUNT		; Loope the counter
         CTRDONE:
             RTS ; Return to caller
-
+        
+        ; Assumes single byte to convert in A register
+        BINTOBCD:
+            PHA ; Save off A
+            ; Zero out BCDVAL for new calculation
+            LDA #0
+            STA BCDVAL
+            STA BCDVAL+1
+            PLA ; Reload with original value
+        CONVERT2BCD:
+            CMP #9
+            BCS ADD9TOBCD; A is higher than 9
+            ; A is equal to or less than 9
+            SED ; Decimal mode
+            CLC ; Clear any latent carry flag
+            ADC BCDVAL ; Add what remains in A to BCDVAL
+            STA BCDVAL
+            LDA #0
+            ADC BCDVAL+1 ; Add carry bit if any
+            STA BCDVAL+1
+            CLD ; Return to normal mode
+            RTS ; A is drained
+        ADD9TOBCD:
+            PHA ; Save original value of A   
+            SED ; Decimal mode
+            CLC ; Clear any latent carry flag
+            LDA BCDVAL
+            ADC #9
+            STA BCDVAL
+            LDA BCDVAL+1
+            ADC #0 ; Carry bit only
+            STA BCDVAL+1
+            CLD ; Normal mode
+            PLA ; Restore A
+            SEC
+            SBC #9 ; Remove 9
+            JMP CONVERT2BCD ; Loop
+            
 ; Multiply, adapted from the 65xx programmers reference
 ; result: returned in X - Y (hi - lo)
 ; All registers are overwritten , caller must save before call
