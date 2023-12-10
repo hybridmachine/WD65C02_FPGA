53c6d6c3d43d77e74f03754ca89fd5e01d0ba2c9
Brian Tabone <brian_tabone@yahoo.com>
2023-12-10 12:16:26 -0600

    Updating doxygen docs

--- docs/html/search/classes_3.js
+++ docs/html/search/classes_3.js
@@ -5,5 +5,7 @@ var searchData=
   ['design_5fwith_5flogic_5fprobe_5fila_5f0_5f0_5farch_2',['design_with_logic_probe_ila_0_0_arch',['../d8/d52/classdesign__with__logic__probe__ila__0__0_1_1design__with__logic__probe__ila__0__0__arch.html',1,'design_with_logic_probe_ila_0_0']]],
   ['design_5fwith_5flogic_5fprobe_5fwd6502_5finterface_5f0_5f1_3',['design_with_logic_probe_WD6502_Interface_0_1',['../de/d34/classdesign__with__logic__probe___w_d6502___interface__0__1.html',1,'']]],
   ['design_5fwith_5flogic_5fprobe_5fwd6502_5finterface_5f0_5f1_5farch_4',['design_with_logic_probe_WD6502_Interface_0_1_arch',['../d5/d50/classdesign__with__logic__probe___w_d6502___interface__0__1_1_1design__with__logic__probe___w_d6502___interface__0__1__arch.html',1,'design_with_logic_probe_WD6502_Interface_0_1']]],
-  ['design_5fwith_5flogic_5fprobe_5fwrapper_5',['design_with_logic_probe_wrapper',['../df/de7/classdesign__with__logic__probe__wrapper.html',1,'']]]
+  ['design_5fwith_5flogic_5fprobe_5fwdc65c02_5finterface_5f0_5f0_5',['design_with_logic_probe_WDC65C02_Interface_0_0',['../d4/d33/classdesign__with__logic__probe___w_d_c65_c02___interface__0__0.html',1,'']]],
+  ['design_5fwith_5flogic_5fprobe_5fwdc65c02_5finterface_5f0_5f0_5farch_6',['design_with_logic_probe_WDC65C02_Interface_0_0_arch',['../d9/dd8/classdesign__with__logic__probe___w_d_c65_c02___interface__0__0_1_1design__with__logic__probe___ed03440f10e10a0cfed67be0b0b5d825.html',1,'design_with_logic_probe_WDC65C02_Interface_0_0']]],
+  ['design_5fwith_5flogic_5fprobe_5fwrapper_7',['design_with_logic_probe_wrapper',['../df/de7/classdesign__with__logic__probe__wrapper.html',1,'']]]
 ];
