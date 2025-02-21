9664bf03c5355263c594533551fc0c4c96a71155
Brian Tabone <brian_tabone@yahoo.com>
2025-02-20 23:13:03 -0600

    Adding more implementation of the IRQ state machine, fixed syntax error , system compiles again

--- WD6502 Computer.srcs/sources_1/new/PIO_INTERRUPT_CONTROLLER.vhd
+++ WD6502 Computer.srcs/sources_1/new/PIO_INTERRUPT_CONTROLLER.vhd
@@ -32,6 +32,10 @@ use work.INTERRUPT_CONTROLLER.ALL;
 --use UNISIM.VComponents.all;
 
 entity PIO_INTERRUPT_CONTROLLER is
+    generic (
+        IRQ_TRIGGER_HOLD_CYCLES : natural := 20;
+        IRQ_ACK_TIMEOUT_CYCLES  : natural := 50_000_000 -- Give the CPU 1/2 second to ack (assume 100mhz clock
+    );
     port(
         clk : in STD_LOGIC;
         irq_to_cpu : out STD_LOGIC; -- Signal line that routes to CPUs IRQ line
@@ -43,23 +47,28 @@ end PIO_INTERRUPT_CONTROLLER;
 
 architecture Behavioral of PIO_INTERRUPT_CONTROLLER is
     signal irq_controller_state : interrupt_controller_state_t := idle;
-    constant IRQ_TRIGGER_HOLD_CYCLES : natural := 20;
+    signal mem_active_irq_signal : STD_LOGIC_VECTOR(7 downto 0); 
 begin
     irq_fsm : process (clk)
     variable irq_trigger_hold_timer : natural := IRQ_TRIGGER_HOLD_CYCLES;
+    variable irq_ack_timeout_timer : natural := IRQ_ACK_TIMEOUT_CYCLES;
+    
     begin
         case irq_controller_state is
             when idle =>
                 mem_active_irq <= IRQNONE;
                 irq_trigger_hold_timer := IRQ_TRIGGER_HOLD_CYCLES;
+                irq_ack_timeout_timer := IRQ_ACK_TIMEOUT_CYCLES;
+                
                 irq_to_cpu <= IRQ_UNTRIGGERED;
                 if (irq_request_vec /= x"00") then
                     -- One or more lines is requested, move to sending request
-                    EnqueueHighestPriorityInterrupt(mem_active_irq, irq_request_vec);
+                    EnqueueHighestPriorityInterrupt(mem_active_irq_signal, irq_request_vec);
                     irq_controller_state <= sending_interrupt;
                 end if;
             when sending_interrupt =>     
                 if (irq_trigger_hold_timer > 0) then
+                    mem_active_irq <= mem_active_irq_signal;
                     irq_to_cpu <= IRQ_TRIGGERED;
                     irq_trigger_hold_timer := irq_trigger_hold_timer - 1;
                     irq_controller_state <= sending_interrupt;
@@ -68,7 +77,18 @@ begin
                     irq_controller_state <= waiting_for_ack;
                 end if;
             when waiting_for_ack =>
+                if (irq_ack_timeout_timer > 0) then
+                    irq_ack_timeout_timer := irq_ack_timeout_timer - 1;
+                else
+                    irq_controller_state <= interrupt_expired;
+                end if;
                 
+                if (mem_active_irq_ack = mem_active_irq_signal) then
+                    irq_controller_state <= interrupt_complete;
+                end if;
+                
+            when interrupt_expired =>
+                irq_controller_state <= interrupt_complete;
             when interrupt_complete =>
                 irq_controller_state <= idle;
             when others =>
--- WD6502 Computer.srcs/sources_1/new/PKG_INTERRUPT_CONTROLLER.vhd
+++ WD6502 Computer.srcs/sources_1/new/PKG_INTERRUPT_CONTROLLER.vhd
@@ -52,7 +52,7 @@ package INTERRUPT_CONTROLLER is
     constant IRQ_REQUESTED : std_logic := '1'; -- We expect drivers to raise their line to high when requesting IRQ
     constant IRQ_STANDBY : std_logic := '0'; -- Drivers pull their lines low when no IRQ requested
 
-    TYPE interrupt_controller_state_t IS (idle, sending_interrupt, waiting_for_ack, interrupt_complete);
+    TYPE interrupt_controller_state_t IS (idle, sending_interrupt, waiting_for_ack, interrupt_complete, interrupt_expired);
    
     
     -- Device driver calls this to raise the interrupt
@@ -65,14 +65,14 @@ package INTERRUPT_CONTROLLER is
     -- Memory manager will call this when processor updates an IRQ mask
     -- When set to 1, any interrupts will be masked and the IRQ manager will 
     -- notify the driver by setting the is_masked bit on the EnqueueHighestPriorityInterrupt call
-    procedure SetIRQMask(signal irq_number : in std_logic_vector(7 downto 0); 
-                         signal irq_mask_vector : out std_logic_vector(15 downto 0);
-                         signal irq_mask_state : in std_logic);
+    --procedure SetIRQMask(signal irq_number : in std_logic_vector(7 downto 0); 
+    --                     signal irq_mask_vector : out std_logic_vector(15 downto 0);
+    ---                     signal irq_mask_state : in std_logic);
 
 
     -- Device will call this to get its IRQ mask state
-    procedure GetIRQMask(signal irq_number : in std_logic_vector(7 downto 0); 
-                         signal irq_mask_state : out std_logic);
+    --procedure GetIRQMask(signal irq_number : in std_logic_vector(7 downto 0); 
+    --                     signal irq_mask_state : out std_logic);
 
 end package INTERRUPT_CONTROLLER;
 
@@ -86,35 +86,35 @@ package body INTERRUPT_CONTROLLER is
         if (irq_vector(0) = IRQ_REQUESTED) then
             irq_number <= IRQ0;
         elsif (irq_vector(1) = IRQ_REQUESTED) then
-            irq_numver <= IRQ1;
+            irq_number <= IRQ1;
         elsif (irq_vector(2) = IRQ_REQUESTED) then
-            irq_numver <= IRQ2;
+            irq_number <= IRQ2;
         elsif (irq_vector(3) = IRQ_REQUESTED) then
-            irq_numver <= IRQ3;
+            irq_number <= IRQ3;
         elsif (irq_vector(4) = IRQ_REQUESTED) then
-            irq_numver <= IRQ4;
+            irq_number <= IRQ4;
         elsif (irq_vector(5) = IRQ_REQUESTED) then
-            irq_numver <= IRQ5;
+            irq_number <= IRQ5;
         elsif (irq_vector(6) = IRQ_REQUESTED) then
-            irq_numver <= IRQ6;
+            irq_number <= IRQ6;
         elsif (irq_vector(7) = IRQ_REQUESTED) then
-            irq_numver <= IRQ7;
+            irq_number <= IRQ7;
         elsif (irq_vector(8) = IRQ_REQUESTED) then
-            irq_numver <= IRQ8;
+            irq_number <= IRQ8;
         elsif (irq_vector(9) = IRQ_REQUESTED) then
-            irq_numver <= IRQ9;
+            irq_number <= IRQ9;
         elsif (irq_vector(10) = IRQ_REQUESTED) then
-            irq_numver <= IRQ10;
+            irq_number <= IRQ10;
         elsif (irq_vector(11) = IRQ_REQUESTED) then
-            irq_numver <= IRQ11;
+            irq_number <= IRQ11;
         elsif (irq_vector(12) = IRQ_REQUESTED) then
-            irq_numver <= IRQ12;
+            irq_number <= IRQ12;
         elsif (irq_vector(13) = IRQ_REQUESTED) then
-            irq_numver <= IRQ13;
+            irq_number <= IRQ13;
         elsif (irq_vector(14) = IRQ_REQUESTED) then
-            irq_numver <= IRQ14;
+            irq_number <= IRQ14;
         elsif (irq_vector(15) = IRQ_REQUESTED) then
-            irq_numver <= IRQ15;
+            irq_number <= IRQ15;
         end if;
     end procedure;
     
