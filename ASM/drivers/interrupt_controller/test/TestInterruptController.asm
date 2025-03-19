d2d851e5348ab43c43db6df2b09139e967b3ebc4
Brian Tabone <brian_tabone@yahoo.com>
2025-03-18 22:37:38 -0500

    WIP adding interrupt controller test code

---
+++ ASM/drivers/interrupt_controller/test/TestInterruptController.asm
@@ -0,0 +1,116 @@
+;***************************************************************************
+;  FILE_NAME: TestInterruptController.asm
+;
+;	Copyright (c) 2025 Brian Tabone
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+; 
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+; 
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+;
+;  DESCRIPTION: Set the timer period and start timer which will fire the IRQ every MS (as programmed). Our interrupt service
+;  routine will then read the timer value and write it to the seven segment display
+;
+;
+;***************************************************************************    
+
+CODE
+; Build as relocatable, specify start address in linker options, see make.bat for start address
+    CHIP	65C02
+    LONGI	OFF
+    LONGA	OFF
+
+;***************************************************************************
+;                             Include Files
+;***************************************************************************
+;None
+
+
+;***************************************************************************
+;                              Global Modules
+;***************************************************************************
+;None
+
+;***************************************************************************
+;                              External Modules
+;***************************************************************************
+;None
+
+;***************************************************************************
+;                              External Variables
+;***************************************************************************
+;None
+
+
+;***************************************************************************
+;                               Local Constants
+;***************************************************************************
+;
+
+START:
+		sei             ; Ignore maskable interrupts
+        clc             ; Clear carry
+    	cld             ; Clear decimal mode
+
+		ldx	#$ff		; Initialize the stack pointer
+		txs
+
+;***************************************************************************
+;                               Application Code
+;***************************************************************************
+;
+
+        ; TODO
+        ; 1) Program the timer period in MS
+        ; 2) Unmask interrupts
+        ; 3) Start the timer
+        ; 4) In interrupt service routine, read timer value on every fired interrupt
+        ; 5) Write timer value to seven segment display -- This might be a simple incremented index
+        ; 6) Write ACK to IRQ controller, in interrupt handler
+
+;This code is here in case the system gets an NMI.  It clears the intterupt flag and returns.
+unexpectedInt:		; $FFE0 - IRQRVD2(134)
+	php
+	pha
+	lda #$FF
+	
+	;clear Irq
+	pla
+	plp
+	rti
+
+IRQHandler:
+		pla
+		rti
+
+	bits:	db	1
+	cnt:	db	0
+	wraps:	dw	0
+	delay:	db	10
+
+
+;***************************************************************************
+vectors	SECTION OFFSET $FFFA
+					;65C02 Interrupt Vectors
+					; Common 8 bit Vectors for all CPUs
+
+		dw	unexpectedInt		; $FFFA -  NMIRQ (ALL)
+		dw	START		        ; $FFFC -  RESET (ALL)
+		dw	IRQHandler      	; $FFFE -  IRQBRK (ALL)
+
+    ends
+
+END ; CODE
